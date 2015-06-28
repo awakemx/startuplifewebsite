2f70d606030365be4b8b01b1aa119d837008f81b
Ismael G Marin C <ismael.marin@gmail.com>
2015-06-27 19:59:13 -0500

    Startup Weekend in time

--- app/models/startup_weekend.rb
+++ app/models/startup_weekend.rb
@@ -1,8 +1,8 @@
 class StartupWeekend < ActiveRecord::Base
   extend FriendlyId
   friendly_id :titulo, use: :slugged
-  default_scope ->{ order(:titulo) }
-  scope :futuro, -> { where("? < ?", Time.now.to_date, :fecha)}
-  scope :presente, -> { where("? >= ? && ? <= ?", Time.now.to_date, :fecha, Time.now.to_date, :fecha)}
-  scope :pasado, -> { where("? > ?", Time.now.to_date, :fecha)}
+  default_scope     ->{ order(:titulo) }
+  scope :futuro,    ->{ where("fecha > ?", Date.today) }
+  scope :presente,  ->{ where('fecha = ? AND fecha <= ?', Date.today, Date.today + 2.days) }
+  scope :pasado,    ->{ where("fecha < ?", Date.today) }
 end
