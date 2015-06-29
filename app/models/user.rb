27a822dd298b1cf62e9c478a7dd8103620d0efae
Ismael G Marin C <ismael.marin@gmail.com>
2015-06-29 01:16:15 -0500

    Gems and base for kaminari and rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@
 #
 
 class User < ActiveRecord::Base
+  rolify
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable ,:confirmable
--- config/application.rb
+++ config/application.rb
@@ -1,21 +1,12 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Startuplife
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
     config.middleware.use Rack::GoogleAnalytics, :tracker => 'UA-64520269-1'
     config.time_zone = 'Central Time (US & Canada)'
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.i18n.default_locale = 'es-MX'
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
